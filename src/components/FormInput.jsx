197578768fbbd36054fdd8fa42a576a0b3883665
monibbormon <monibbormon14@gmail.com>
2024-10-05 23:24:58 +0600

    edit form

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,16 +1,37 @@
-const FormInput = ({ label, type, placeholder, value, isEdit = false }) => {
+const FormInput = ({
+  label,
+  type,
+  placeholder,
+  value,
+  className,
+  isEdit = false,
+}) => {
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // Month is 0-indexed
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  };
+
+  const formattedValue = type === "date" && value ? formatDate(value) : value;
   return (
-    <div>
-      <label htmlFor="" className="text-[12px] font-medium text-gray-400 mb-2">
+    <div className={className}>
+      <label
+        htmlFor=""
+        className={`text-[12px] font-medium text-gray-400 inline-block ${
+          isEdit && "text-gray-900 text-sm mb-2"
+        }`}
+      >
         {label} {isEdit && <sup className="text-red-500">*</sup>}
       </label>{" "}
       <br />
       {isEdit ? (
         <input
-          className="h-[37px] rounded-[8px] p-[8px_16px] border focus:outline-primary-600"
+          className="h-[37px] rounded-[8px] p-[8px_16px] focus:outline-primary-600 outline-[1px] border-[1px] border-gray-300 bg-gray-50 text-sm"
           type={type ? type : "text"}
           placeholder={placeholder}
-          value={value}
+          defaultValue={formattedValue}
         />
       ) : (
         <h4 className="text-gray-900 text-sm mt-3">{value}</h4>
--- src/components/applications-details/ApplicantBasicInfo.jsx
+++ src/components/applications-details/ApplicantBasicInfo.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import FormInput from "../FormInput";
 import ApplicantPhoto from "./ApplicantPhoto";
 
 const ApplicantBasicInfo = () => {
+  const [isEdit, setIsEdit] = useState(false);
   return (
     <div className="grid grid-cols-[55%_45%] gap-x-6">
       <div className="border-[1px] flex-1 border-[#E5E5E5] p-[24px] rounded-[16px]">
@@ -10,20 +12,53 @@ const ApplicantBasicInfo = () => {
             Basic Information
           </h2>
 
-          <button className="border-[1px] border-gray-200 text-gray-900 p-[8px_12px] rounded-[32px] font-medium text-[12px]">
-            Edit
-          </button>
+          {isEdit ? (
+            <div className="flex flex-col 2xl:flex-row gap-y-2 items-center gap-x-4">
+              <button
+                onClick={() => setIsEdit(false)}
+                className="border-[1px] border-gray-200 text-gray-900 p-[8px_12px] rounded-[32px] font-medium text-[12px]"
+              >
+                Undo changes
+              </button>
+              <button
+                onClick={() => setIsEdit(false)}
+                className="border-[1px] border-primary-700 text-primary-700 p-[8px_12px] rounded-[32px] font-medium text-[12px]"
+              >
+                Submit
+              </button>
+            </div>
+          ) : (
+            <button
+              onClick={() => setIsEdit(true)}
+              className="border-[1px] border-gray-200 text-gray-900 p-[8px_12px] rounded-[32px] font-medium text-[12px]"
+            >
+              Edit
+            </button>
+          )}
         </div>
 
-        <div className="grid grid-cols-2 mb-5 border-b border-gray-50 pb-3">
-          <FormInput label="First Name" placeholder="First Name" value="Anik" />
-          <FormInput label="Last Name" placeholder="First Name" value="Molla" />
+        <div className="grid 2xl:grid-cols-2 mb-5 border-b border-gray-50">
+          <FormInput
+            label="First Name"
+            placeholder="First Name"
+            value="Anik"
+            isEdit={isEdit}
+            className="pb-3"
+          />
+          <FormInput
+            label="Last Name"
+            placeholder="Last Name"
+            value="Molla"
+            isEdit={isEdit}
+            className="pb-3"
+          />
         </div>
         <div className="mb-5 border-b border-gray-50 pb-3">
           <FormInput
             label="Father Name"
             placeholder="Father Name"
             value="Anika"
+            isEdit={isEdit}
           />
         </div>
         <div className="mb-5 border-b border-gray-50 pb-3">
@@ -31,33 +66,42 @@ const ApplicantBasicInfo = () => {
             label="Mother Name"
             placeholder="Mother Name"
             value="Anika"
+            isEdit={isEdit}
           />
         </div>
         <div className="mb-5 border-b border-gray-50 pb-3">
           <FormInput
             label="Date of Birth"
-            placeholder="First Name"
             value="5-June-1998"
+            type="date"
+            isEdit={isEdit}
           />
         </div>
         <div className="mb-5 border-b border-gray-50 pb-3">
-          <FormInput label="Gender" placeholder="First Name" value="Male" />
+          <FormInput label="Gender" placeholder="First Name" value="Male" isEdit={isEdit} />
         </div>
         <div className="mb-5 border-b border-gray-50 pb-3">
           <FormInput
             label="Nationality"
             placeholder="First Name"
             value="Nepal"
+            isEdit={isEdit}
           />
         </div>
         <div className="mb-5 border-b border-gray-50 pb-3">
-          <FormInput label="Position" placeholder="First Name" value="Rider" />
+          <FormInput
+            label="Position"
+            placeholder="First Name"
+            value="Rider"
+            isEdit={isEdit}
+          />
         </div>
         <div className="mb-5 border-b border-gray-50 pb-3">
           <FormInput
             label="Reference Number"
             placeholder="First Name"
             value="4552212"
+            isEdit={isEdit}
           />
         </div>
       </div>
