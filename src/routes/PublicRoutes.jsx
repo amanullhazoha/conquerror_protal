a7e2aef1945eb7579e4630b773af3bb796743bea
kazi331 <kazisharifulislam52@gmail.com>
2024-12-10 09:07:07 +0600

    interview added

--- src/routes/PublicRoutes.jsx
+++ src/routes/PublicRoutes.jsx
@@ -1,12 +1,10 @@
-import Login from "@/pages/auth/Login";
-import SignUp from "@/pages/auth/SignUp";
-import { Outlet } from "react-router-dom";
-import CheckEmail from "@/pages/auth/CheckEmail";
-import ResetPassword from "@/pages/auth/ResetPassword";
-import ForgotPassword from "@/pages/auth/ForgotPassword";
 import PublicRoute from "@/components/guards/PublicRoute";
+import CheckEmail from "@/pages/auth/CheckEmail";
 import ForgotPasswordStepper from "@/pages/auth/ForgotPasswordStepper";
+import Login from "@/pages/auth/Login";
 import PasswordResetConfirmation from "@/pages/auth/PasswordResetConfirmation";
+import ResetPassword from "@/pages/auth/ResetPassword";
+import { Outlet } from "react-router-dom";
 
 const PublicRoutes = {
   path: "/",
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,19 @@
-import GlobalRoutes from "./GlobalRoutes";
-import PublicRoutes from "./PublicRoutes";
+import ScheduleInterview from "@/components/ScheduleInterview";
 import { createBrowserRouter } from "react-router-dom";
 import AuthenticatedRoutes from "./AuthenticatedRoutes";
+import GlobalRoutes from "./GlobalRoutes";
+import PublicRoutes from "./PublicRoutes";
 
 const router = createBrowserRouter(
-  [PublicRoutes, AuthenticatedRoutes, GlobalRoutes],
+  [
+    PublicRoutes,
+    AuthenticatedRoutes,
+    GlobalRoutes,
+    {
+      path: "/interview",
+      element: <ScheduleInterview />,
+    }
+  ],
   {
     basename: "/",
   }
